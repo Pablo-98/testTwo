b95670d3d6bac3deb0fd4ef21c5e5ffe8d89a1bf
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-10-20 15:22:06 -0600

    my first commit

---
+++ test.py
@@ -0,0 +1,26 @@
+import random;
+
+def main():
+     myMethod();
+     myMethodTwo();
+     myMethodThree();
+
+
+def myMethod():
+    print("hello world");
+
+
+def myMethodTwo():
+     myVar = input("enter a name")
+     print(myVar);
+
+def myMethodThree():
+     input = input("enter an integer:)");
+     number = random.randInt(1,11);
+     if input == number:
+          print("you got it");
+    
+
+if (1 == 1):
+    main();
+    
